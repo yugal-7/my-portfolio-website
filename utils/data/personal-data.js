ea4e3faadc3e218e5f1715bd45e45418f2f82a9d
Yugal Vashishth <yugalvashishth@Yugals-Laptop.local>
2024-11-04 22:39:00 +0530

    refactor: changed info

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,13 +2,13 @@ export const personalData = {
   name: "YUGAL",
   profile: '/profile.png',
   designation: "Software Engineer",
-  description: "My name is Yugal Vashishth. I am a professional and enthusiastic programmer. I am quick learner with self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Full-stack developer with 2.6 years of experience in building scalable web applications using DynamoDB, MongoDB, Express, React, Node, Next. Proficient in developing efficient APIs, implementing microservices architecture, and optimizing front-end performance.",
   email: 'yugal17v@gmail.com',
   phone: '+91 9560282586',
   address: 'Faridabad',
   github: 'https://github.com/yugal-7',
   linkedIn: 'https://www.linkedin.com/in/yugal-vashishth-65a23718b/',
-  twitter: 'https://twitter.com/YUGAL63023269',
   leetcode: "https://leetcode.com/yugal17v/",
-  resume: "https://drive.google.com/file/d/12ZVMPWsEUYM8M2J0MbTTtvJmIBwxZV2u/view?usp=sharing"
+  twitter: 'https://twitter.com',
+  resume: "https://drive.google.com/file/d/192hBVY9NusBIpjgcrB0wk6f7qJiQAsIF/view?usp=sharing"
 }
